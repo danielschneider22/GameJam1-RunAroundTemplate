eb37b6a706e238ccba23fbdbefccacd19b701665
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-02-21 20:53:22 -0500

    timer

---
+++ Assets/Scripts/Timers/GameEndTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameEndTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+    void Start()
+    {
+        timer = mainTimer;
+    }
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("F");
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            timerText.text = "0.00";
+            timer = 0.0f;
+        }
+    }
+}
