bbec76082e735b6fbb584c9e876450a2a8bb13b3
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-02-23 02:11:13 -0500

    more fixes

---
+++ Assets/Scripts/Player/PlayerSpeak.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpeak : MonoBehaviour
+{
+    public SpriteRenderer speechBubble;
+    public Sprite laugh;
+    public Sprite music;
+    public Sprite star;
+
+    private AudioManager audioManager;
+
+    public void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.A))
+       {
+            speechBubble.sprite = laugh;
+            audioManager.Play("Laughing");
+       }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            speechBubble.sprite = music;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            speechBubble.sprite = star;
+        }
+    }
+}
